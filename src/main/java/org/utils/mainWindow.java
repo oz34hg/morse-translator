eaf6f6900405ab741ed8db631383aa55fc8a9232
oreste <orestelaratorresox@gmail.com>
2026-03-24 20:49:15 -0700

    Added mobility between layouts and new buttons for future use
    main functionalities to be implemented in following versions

--- src/main/java/org/utils/mainWindow.java
+++ src/main/java/org/utils/mainWindow.java
@@ -2,7 +2,6 @@ package org.utils;
 
 import javax.swing.*;
 import java.awt.*;
-import org.utils.modernButton;
 
 
 public class mainWindow extends JFrame{
@@ -15,11 +14,21 @@ public class mainWindow extends JFrame{
         // create buttons for main panel
         JButton b1 = new JButton("Translate to Morse");
         JButton b2 = new JButton("Translate from Morse");
+        JButton b3 = new JButton("MENU");
+        JButton b4 = new JButton("MENU");
+
+        JButton translateTo = new JButton("Translate");
+        JButton translateFrom = new JButton("Translate");
+
         // set design for buttons
+
 
         JPanel mainPanel = new JPanel();
         JPanel toMorse = new JPanel();
         JPanel fromMorse = new JPanel();
+
+        toMorse.setLayout(new GridLayout());
+        fromMorse.setLayout(new GridLayout());
 
         // sets size and place for the mainPanel
         mainPanel.setSize(800, 800);
@@ -33,7 +42,7 @@ public class mainWindow extends JFrame{
 
         mainPanel.add(b1);
         mainPanel.add(b2);
-        mainPanel.add(b3);
+
 
         // add panels to deck layout
         deck.add(mainPanel, "mainPage");
@@ -41,6 +50,18 @@ public class mainWindow extends JFrame{
         deck.add(fromMorse, "TranslateFromMorse");
 
 
+        this.add(deck);
+
+        // add home button to both layouts
+        toMorse.add(b3);
+        fromMorse.add(b4);
+
+
+        // handle the clicks on the buttons
+        b1.addActionListener(e -> layoutCards.show(deck, "TranslateToMorse"));
+        b2.addActionListener(e -> layoutCards.show(deck, "TranslateFromMorse"));
+        b3.addActionListener(e -> layoutCards.show(deck, "mainPage"));
+        b4.addActionListener(e -> layoutCards.show(deck, "mainPage"));
 
         // sets title for the window
         this.setTitle("MORSE TRANSLATOR");
